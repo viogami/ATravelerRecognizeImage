01c0b535fcb1f7e744380e0eeae2787e299bc95e
cyw <2654613995@qq.com>
2022-10-31 16:52:13 +0800

    first commit

---
+++ ATRI.py
@@ -0,0 +1,125 @@
+import os
+import pyautogui
+import time
+import xlrd
+from gooey import Gooey, GooeyParser
+import mainwork
+
+# 定义数据检查
+# cmdType.value  1.0 左键单击    2.0 左键双击  3.0 右键单击  4.0 输入  5.0 等待  6.0 滚轮 
+# ctype     空：0
+#           字符串：1
+#           数字：2
+#           日期：3
+#           布尔：4
+#           error：5
+def dataCheck(sheet1):
+    checkCmd = True
+    #行数检查
+    if sheet1.nrows<2:
+        print("没有输入操作内容")
+        checkCmd = False
+    #每行数据检查
+    i = 1
+    while i < sheet1.nrows:
+        # 第1列 操作类型检查
+        cmdType = sheet1.row(i)[0]
+        if (cmdType.value != 1.0 and cmdType.value != 2.0 and cmdType.value != 3.0 
+            and cmdType.value != 4.0 and cmdType.value != 5.0 and cmdType.value != 6.0
+            and cmdType.value !=7.0):
+            print('第1列，第',i+1,"行,请输入正确的操作类型（数字）")
+            checkCmd = False
+        # 第2列 内容检查
+        cmdValue = sheet1.row(i)[1]
+        # 读图点击类型指令，内容必须为字符串类型
+        if cmdType.value ==1.0 or cmdType.value == 2.0 or cmdType.value == 3.0:
+            if cmdValue.ctype != 1:
+                print('第2列，第',i+1,"行,数据输入错误")
+                checkCmd = False
+        # 输入类型，内容不能为空
+        if cmdType.value == 4.0:
+            if cmdValue.ctype == 0:
+                print('第2列，第',i+1,"行,数据输入错误")
+                checkCmd = False
+        # 等待类型，内容必须为数字
+        if cmdType.value == 5.0:
+            if cmdValue.ctype != 2:
+                print('第2列，第',i+1,"行,数据输入错误")
+                checkCmd = False
+        # 滚轮事件，内容必须为数字
+        if cmdType.value == 6.0:
+            if cmdValue.ctype != 2:
+                print('第2列，第',i+1,"行,数据输入错误")
+                checkCmd = False
+        i += 1
+    return checkCmd
+
+     
+
+
+#定义判断图像是否存在 参数：img:图片位置或名称  confidence:相似度
+def imgexist(img,similar):
+    location=pyautogui.locateCenterOnScreen(img,confidence=similar)
+    if location is not None:
+        return True
+    elif location is None:
+        return False
+
+#定义读取excel文件的函数，返回sheet1的表格内容
+def readexcel(filepath):
+    #获得路径下的文件名称(带后缀)
+    f=os.path.basename(filepath)
+    #用xlrd库打开excel文件
+    wb = xlrd.open_workbook(f)
+    #通过索引获取表格sheet页，只需要sheet1即可
+    sheet1 = wb.sheet_by_index(0)
+    return sheet1
+
+@Gooey(
+    richtext_controls=True,  # 打开终端对颜色支持
+    program_name="ATravelerRecognizeImage",  # 程序名称
+    encoding="utf-8",  # 设置编码格式，打包的时候遇到问题
+    #设置菜单
+    menu=[{
+           'name': '菜单',
+           'items': [{
+               'type': 'AboutDialog',
+               'menuTitle': '关于',
+               'name': 'ATravelerRecongnizeImage',
+               'description': '作者:kagami',
+               'version': 'v1.1',
+               'website': 'https://github.com/Violetmail/ATravelerRecognizeImage'}]
+       }
+       ])
+def main():
+    #设置图形化界面
+    parser = GooeyParser(description="基于图像匹配的自动操作脚本") 
+    parser.add_argument('Excelpath', metavar="Excel路径", widget="FileChooser",default="D:\GitHub\ATravelerRecognizeImage\\test1.xlsx")
+    parser.add_argument('Imagepath', metavar="图片文件夹", widget="DirChooser",default="D:\GitHub\ATravelerRecognizeImage\\image")
+    parser.add_argument('-loop', help="是否无限循环?",metavar="循环",widget="CheckBox",action="store_true")
+    parser.add_argument('-skip', help="十次未匹配不到图片跳过该图",metavar="跳过",widget="CheckBox",action="store_true")
+    args = parser.parse_args()
+
+    ##start##
+    print('欢迎使用,萝卜子开始运行...')
+    sheet1=readexcel(args.Excelpath)
+    #数据检查
+    checkCmd = dataCheck(sheet1)
+    if checkCmd:
+        if args.loop:
+             #循环，无限运行
+            while True:
+                mainwork.mainWork(sheet1,args.Imagepath,args.skip)
+                time.sleep(0.1)
+                print("等待0.1秒")
+        else:
+            #不循环，运行一次
+            mainwork.mainWork(sheet1,args.Imagepath,args.skip)
+            print("成功运行流程一次！")  
+    else:
+        print('Excel表格数据输入有误!')
+    
+
+if __name__ == '__main__':
+    main()
+##
---
+++ mainwork.py
@@ -0,0 +1,106 @@
+import pyautogui
+import time
+import pyperclip
+
+#定义鼠标事件
+def mouseClick(clickTimes,lOrR,img,reTry):
+    if reTry == 1:
+        while True:
+            location=pyautogui.locateCenterOnScreen(img,confidence=0.9)
+            if location is not None:
+                pyautogui.click(location.x,location.y,clicks=clickTimes,interval=0.2,duration=0.2,button=lOrR)
+                break
+            print("未匹配到图片"+img+",0.1秒后重试")
+            time.sleep(0.1)
+    elif reTry == -1:
+        while True:
+            location=pyautogui.locateCenterOnScreen(img,confidence=0.9)
+            if location is not None:
+                pyautogui.click(location.x,location.y,clicks=clickTimes,interval=0.2,duration=0.2,button=lOrR)
+            print("未匹配到图片"+img+",0.1秒后重试")
+            time.sleep(0.1)
+    elif reTry > 1:
+        i = 1
+        while i < reTry + 1:
+            location=pyautogui.locateCenterOnScreen(img,confidence=0.9)
+            if location is not None:
+                pyautogui.click(location.x,location.y,clicks=clickTimes,interval=0.2,duration=0.2,button=lOrR)
+                print("0.1秒后重复执行一次！")
+                i += 1
+            time.sleep(0.1)
+    #retry=0,意味着多次匹配不到可以跳过
+    elif reTry==0:
+        location=pyautogui.locateCenterOnScreen(img,confidence=0.9)
+        i=0
+        while i<10:
+            if location is not None:
+                pyautogui.click(location.x,location.y,clicks=clickTimes,interval=0.2,duration=0.2,button=lOrR)
+                break
+            i=i+1
+            print("未匹配到图片"+img+",0.2秒后重试")
+            time.sleep(0.2)
+
+
+
+#定义任务 对于参数有： 
+# sheet1:字符型，Excel表格文件位置
+# imgpath：字符型，图片文件位置
+# skip：布尔型，是否retry=0
+
+def mainWork(sheet1,imgpath,skip):
+    i = 1
+    while i < sheet1.nrows:
+        #取本行指令的操作类型
+        cmdType = sheet1.row(i)[0]
+        if cmdType.value == 1.0:
+            #取图片名称
+            img = imgpath+"\\"+sheet1.row(i)[1].value
+            reTry = 1
+            if skip:
+                reTry=0
+            elif sheet1.row(i)[2].ctype == 2 and sheet1.row(i)[2].value != 0:
+                reTry = sheet1.row(i)[2].value
+            mouseClick(1,"left",img,reTry)
+            print("单击左键",img)
+        #2代表双击左键
+        elif cmdType.value == 2.0:
+            #取图片名称
+            img = imgpath+"\\"+sheet1.row(i)[1].value
+            #取重试次数
+            reTry = 1
+            if sheet1.row(i)[2].ctype == 2 and sheet1.row(i)[2].value != 0:
+                reTry = sheet1.row(i)[2].value
+            mouseClick(2,"left",img,reTry)
+            print("左键双击",img)
+        #3代表右键
+        elif cmdType.value == 3.0:
+            #取图片名称
+            img = imgpath+"\\"+ sheet1.row(i)[1].value
+            #取重试次数
+            reTry = 1
+            if sheet1.row(i)[2].ctype == 2 and sheet1.row(i)[2].value != 0:
+                reTry = sheet1.row(i)[2].value
+            mouseClick(1,"right",img,reTry)
+            print("右键单击",img) 
+        #4代表输入
+        elif cmdType.value == 4.0:
+            inputValue = sheet1.row(i)[1].value
+            pyperclip.copy(inputValue)
+            pyautogui.hotkey('ctrl','v')
+            time.sleep(0.5)
+            print("输入:",inputValue)                                        
+        #5代表等待
+        elif cmdType.value == 5.0:
+            #取等待时间
+            waitTime = sheet1.row(i)[1].value
+            time.sleep(waitTime)
+            print("等待",waitTime,"秒")
+        #6代表滚轮
+        elif cmdType.value == 6.0:
+            #取滚动距离
+            scroll = sheet1.row(i)[1].value
+            pyautogui.scroll(int(scroll))
+            print("滚轮滑动，距离：",int(scroll))  
+        elif cmdType.value == 7.0:
+            continue                  
+        i += 1
